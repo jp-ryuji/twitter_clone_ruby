9a1143275e8689ea92eb9e4b5011e11ebd496c93
Ryuji Eguchi <jp.ryuji@gmail.com>
2017-09-16 20:09:48 +0900

    Add flash_messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def flash_messages
+    return if flash.empty?
+    capture do
+      flash.each do |type, message|
+        concat flash_message(type, message)
+      end
+    end
+  end
+
+  def flash_message(type, message)
+    type = bootstrap_alert_class_for(type)
+    content_tag :div, class: "alert #{type} alert-dismissible" do
+      capture do
+        concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
+        concat message
+      end
+    end
+  end
+
+  def bootstrap_alert_class_for(type)
+    case type.to_sym
+    when :success then 'alert-success'
+    when :error then 'alert-danger'
+    when :notice then 'alert-info'
+    when :alert then 'alert-warning'
+    else 'alert-info'
+    end
+  end
 end
