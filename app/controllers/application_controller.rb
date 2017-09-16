701208371702ebe3bafb32b4e705cfe9858fdeb4
Ryuji Eguchi <jp.ryuji@gmail.com>
2017-09-16 12:47:25 +0900

    Add authentication with sorcery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :error
 
   protect_from_forgery with: :exception
+
+  protected
+    def not_authenticated
+      redirect_to login_url, alert: 'You need to login first to access this page.'
+    end
 end
