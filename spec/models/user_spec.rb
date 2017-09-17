9c56876d89beadd91786b632762d38efb0a1934a
Ryuji Eguchi <jp.ryuji@gmail.com>
2017-09-17 09:42:44 +0900

    Add following/followed relations through Following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  it 'has many followers' do
+    user.followers << create_list(:user, 2)
+    expect(user.followers.size).to eq(2)
+  end
+
+  it 'has many followees' do
+    create_list(:user, 2).each { |u| u.followers << user }
+    expect(user.followees.size).to eq(2)
+  end
 end
