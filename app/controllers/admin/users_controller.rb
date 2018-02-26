5bdeb3ba291a2742b8bd55cee3f89ed3b0677163
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-02-26 19:22:34 +0900

    Add admin user pages

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Admin
+  class UsersController < Admin::BaseController
+    before_action :set_user, only: %i[show edit update destroy]
+
+    def index
+      @users = User.order(created_at: :desc).page(params[:page])
+    end
+
+    def show; end
+
+    def new
+      @user = User.new
+    end
+
+    def edit; end
+
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        redirect_to admin_users_url, notice: "User (#{@user.screen_name}) was successfully created."
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_url, notice: "User (#{@user.screen_name}) was successfully updated."
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to admin_users_url, notice: "User (#{@user.screen_name}) was successfully destroyed."
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :screen_name)
+    end
+  end
+end
