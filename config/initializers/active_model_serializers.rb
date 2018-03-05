21098385afe85ff61468f2c8076568fec2f856ba
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-03-05 10:49:08 +0900

    Disable auto key_transform by active_model_serializers

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -4,3 +4,8 @@
 #   By default ActiveModelSerializers will use the Attributes Adapter (no JSON root).
 #   But we strongly advise you to use JsonApi Adapter
 ActiveModelSerializers.config.adapter = :json_api
+# NOTE: AMS converts underscores to hyphens automatically to comply with recommendation of JSON API.
+#   See: http://jsonapi.org/recommendations/#naming
+#   The following setting disables that.
+#   It doesn't mean this is recommendation. fast_jsonapi doesn't do so, for instance.
+ActiveModel::Serializer.config.key_transform = :unaltered
