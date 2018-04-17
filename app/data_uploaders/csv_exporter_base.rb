afa122b492a308ba302f5152075de43d54c138f8
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-17 13:17:03 +0900

    Fix encoding in CsvExporterBase

--- app/data_uploaders/csv_exporter_base.rb
+++ app/data_uploaders/csv_exporter_base.rb
@@ -8,8 +8,6 @@ class CsvExporterBase
   end
 
   def export
-    # NOTE: The encoding is for the Japanese environment.
-    encoding = Encoding::Windows_31J
     rows = CSV.generate(row_sep: "\r\n", headers: self.class::COLUMNS, write_headers: true, force_quotes: true) do |csv|
       # NOTE: find_each is used here because @records could be thousands of records. If there's no such concern, use each instead.
       @records.find_each do |record|
@@ -17,7 +15,8 @@ class CsvExporterBase
       end
     end
 
-    rows.encode(encoding, invalid: :replace, undef: :replace)
+    # NOTE: The encoding is for the Japanese environment.
+    rows.encode(Encoding::Windows_31J, invalid: :replace, undef: :replace)
   end
 
   private
